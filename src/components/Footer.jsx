1f5e60575ac0073c88515086d900c6d207de2938
SHIVADVAIT <156700666+SHIVADVAIT@users.noreply.github.com>
2025-05-30 23:19:12 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-marquee">
-        राम नाम की लूट है, लूट सके तो लूट।
-        अंत काल पछताएगा, जब प्राण जाएँगे छूट॥
-      </div>
+    
 
       <div className="footer-container">
         <p>© {new Date().getFullYear()} गुरुदेव — सभी अधिकार सुरक्षित 🙏</p>
